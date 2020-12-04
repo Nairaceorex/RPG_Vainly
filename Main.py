56d1526cbb2dc0b15ef1a3e1157be3be931c2da2
Nairaceorex <wwwolf2002@gmail.com>
2020-12-05 02:05:58 +0300

    add character prototype

--- Main.py
+++ Main.py
@@ -1,22 +1,31 @@
-import  pygame
-from Constants import  *
+import pygame
+from Constants import *
+from Person import *
+
+
 class Main:
-    def __init__(self,screen):
-        self.screen=screen
-        self.background=pygame.image.load('Old-Dungeon.jpg')
-        self.running =True
+    def __init__(self, screen):
+        self.screen = screen
+        self.person = Person()
+        self.background = pygame.image.load('Old-Dungeon1.png')
+        self.running = True
         self.main_loop()
         pass
+
     def render(self):
-        self.screen.blit(self.background,(0,0))
+        self.screen.blit(self.background, (0, 0))
+        self.person.render(screen)
         pygame.display.flip()
         pass
+
     def main_loop(self):
-        while self.running==True:
+        while self.running == True:
             self.render()
+
     def handl_events(self):
         pass
 
+
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIGHT,SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIGHT, SCREEN_HEIGHT))
 game = Main(screen)
--- Person.py
+++ Person.py
@@ -1,6 +1,37 @@
+import pygame
+from Constants import *
+
+
 class Person:
     hp = 100
     lvlAttack = 1
     lvlMagic = 1
     lvlDefence = 1
     lvlHeal = 1
+
+    def __init__(self):
+        self.state = ALIVE
+        self.directon = RIGHT
+        self.x=START_X
+        self.y=START_Y
+        self.name = 'Hero'
+        self.hp=HP
+        self.mp=MP
+        self.image_pack = ['playerR.png','playerL.png']
+        self.images = []
+        for image in self.image_pack:
+            temp = pygame.image.load(image).convert_alpha()
+            i = []
+            i.append(temp.subsurface(0, 0, 580, 580))
+            #i.append(temp.subsurface(580, 0, 580, 580))
+            self.images.append(i)
+        self.moving = [0, 0,0]
+
+    def move(self):
+        pass
+
+    def render(self, screen):
+        screen.blit(self.images[self.directon][self.state], (self.x, self.y))
+
+    def render_ui(self, screen):
+        pass
